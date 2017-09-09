63234a7b0d9a26e3db7ec1771db230eb8c016941
Alexander Bondarchuk <al.v.bondarchuk@gmail.com>
2017-09-09 13:31:25 +0200

    initial commit

---
+++ code/main.py
@@ -0,0 +1,141 @@
+import pickle
+import csv
+import tensorflow as tf
+from sklearn.utils import shuffle
+from nn_model_factory import *
+from time_tracker import *
+from input_normalizer import *
+
+
+training_file = "../../traffic-signs-data/train.p"
+validation_file = "../../traffic-signs-data/valid.p"
+testing_file = "../../traffic-signs-data/test.p"
+
+sign_names_file = "../../CarND-Traffic-Sign-Classifier-Project/signnames.csv"
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(validation_file, mode='rb') as f:
+    valid = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+with open(sign_names_file) as f:
+    s_names_dict = {}
+    readCSV = csv.reader(f, delimiter=",")
+    for index, row in enumerate(readCSV):
+        if(index > 0):
+            s_names_dict[int(row[0])] = row[1]
+
+X_train, y_train = train['features'], train['labels']
+X_valid, y_valid = valid['features'], valid['labels']
+X_test, y_test = test['features'], test['labels']
+
+n_train = len(X_train)
+n_validation = len(X_valid)
+n_test = len(X_test)
+image_shape = "{0[1]}x{0[2]}".format(X_train.shape)
+n_classes = len(set(y_train))
+
+print("Number of training examples =", n_train)
+print("Number of testing examples =", n_test)
+print("Image data shape =", image_shape)
+print("Number of classes =", n_classes)
+
+input_normalizer = InputNormalizer()
+n_labels, n_data = input_normalizer.normalize_by_amount(y_train, X_train)
+# n_labels, n_data = y_train, X_train
+
+session = tf.InteractiveSession()
+
+def variable_summaries(var):
+    with tf.name_scope("summaries"):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar("mean", mean)
+    with tf.name_scope("stddev"):
+        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar("stddev", stddev)
+    tf.summary.scalar("max", tf.reduce_max(var))
+    tf.summary.scalar("min", tf.reduce_min(var))
+    tf.summary.histogram("histogram", var)
+
+with tf.name_scope("traffic_signs_input"):
+    im_input = tf.placeholder(tf.uint8, [None, 32, 32, 3], name="im_input")
+    y_ = tf.placeholder(tf.int32, (None,), name="y_")
+    one_hot_y = tf.one_hot(y_, 42, name="one_hot_y")
+    x_ = tf.image.convert_image_dtype(im_input, dtype=tf.float32)
+
+with tf.name_scope("input"):
+    tf.summary.image("images", im_input, 5)
+
+keep_prob = tf.placeholder(tf.float32, name="keep_prob")
+
+model = NnModelFactory().create(x_, keep_prob)
+
+# loss measurement
+with tf.name_scope("cross_entropy"):
+    softmax_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=model)
+    cross_entropy = tf.reduce_mean(softmax_cross_entropy)
+
+# I'm using Adam optimizer instead of gradient descent
+# Adam is the variant of gradient descent optimizer, that varies step size to prevent
+# overshooting a good solution and becoming unstable
+learning_rate = 1e-4
+with tf.name_scope("loss_optimizer"):
+    train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
+
+# accuracy calculation
+with tf.name_scope("acuracy"):
+    prediction_is_correct = tf.equal(tf.arg_max(model, 1), tf.argmax(one_hot_y, 1))
+    accuracy = tf.reduce_mean(tf.cast(prediction_is_correct, tf.float32))
+
+tf.summary.scalar("cross_entropy_scl", cross_entropy)
+tf.summary.scalar("training_acuracy", accuracy)
+
+summarize_all = tf.summary.merge_all()
+
+
+num_steps = len(n_data)
+timer = TimeTracker(num_steps)
+
+# x_data = input_normalizer.normalize_color(n_data)
+# x_data = input_normalizer.image_to_float(n_data)
+
+EPOCHS = 25
+BATCH_SIZE = 20
+
+saver = tf.train.Saver()
+
+session.run(tf.global_variables_initializer())
+timer.log("Global variable initialization")
+
+# ----
+tb_log_path = "./tb_logs/{}-ep_{}-batch_rate-{}_all_data/".format(EPOCHS, BATCH_SIZE, learning_rate)
+model_path = tb_log_path + "model/"
+tb_writer = tf.summary.FileWriter(tb_log_path, session.graph)
+# ----
+index = 0
+
+for epoch in range(EPOCHS):
+    print("Epoch: ", epoch)
+    n_labels, n_data = shuffle(y_train, X_train)
+    #n_labels, n_data = shuffle(n_labels, n_data)
+    #n_labels, n_data = input_normalizer.normalize_by_amount(y_train, X_train)
+
+    for offset in range(0, num_steps, BATCH_SIZE):
+        x_batch, y_batch = n_data[offset:offset + BATCH_SIZE], n_labels[offset:offset + BATCH_SIZE]
+        _, summary = session.run([train_step, summarize_all],feed_dict={im_input: x_batch, y_: y_batch, keep_prob: 0.5})
+
+        train_accuracy = session.run(accuracy, feed_dict={im_input: x_batch, y_: y_batch, keep_prob: 1.0})
+        if offset%1000 == 0:
+            print("training accuracy {:.3f}".format(train_accuracy*100))
+
+        index += len(x_batch)
+        # write summary to log
+        tb_writer.add_summary(summary, index)
+
+
+saver.save(session, model_path)
+timer.log("Session save")
+
+timer.output_summary()
+session.close()
---
+++ code/nn_model_factory.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+
+
+class NnModelFactory:
+
+    @staticmethod
+    def _weight_variable(shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial, name="weight")
+
+    @staticmethod
+    def _bias_variable(shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial, name="bias")
+
+    @staticmethod
+    def _conv2d(x, w):
+        return tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding="VALID", name="conv2d")
+
+    @staticmethod
+    def _max_pool_2x2(x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID", name="max_pool")
+
+    def create(self, x, tf_keep_prob):
+        with tf.name_scope("conv1"):
+            w_conv1 = self._weight_variable([5, 5, 3, 32])
+            b_conv1 = self._bias_variable([32])
+            conv1_wx_b = self._conv2d(x, w_conv1) + b_conv1
+
+            conv1 = tf.nn.relu(conv1_wx_b, name="relu")
+            # Convolution output is 28x28x32
+            pool1 = self._max_pool_2x2(conv1)
+            # Pooling output is 14x14x32
+
+        with tf.name_scope("conv2"):
+            w_conv2 = self._weight_variable([5, 5, 32, 100])
+            b_conv2 = self._bias_variable([100])
+            conv2_wx_b = self._conv2d(pool1, w_conv2) + b_conv2
+
+            conv2 = tf.nn.relu(conv2_wx_b, name="relu")
+            # Convolution output is 10x10x100
+            pool2 = self._max_pool_2x2(conv2)
+            # Pooling output is 5x5x100
+
+        with tf.name_scope("fully_connected"):
+            w_fc1 = self._weight_variable([5 * 5 * 100, 1024])
+            b_fc1 = self._bias_variable([1024])
+
+            pool2_flat = tf.reshape(pool2, [-1, 5 * 5 * 100], name="pool")
+            fc1 = tf.nn.relu(tf.matmul(pool2_flat, w_fc1) + b_fc1, name="relu")
+
+        # dropout
+        fc1_drop = tf.nn.dropout(fc1, keep_prob=tf_keep_prob, name="dropout")
+
+        # readout
+        with tf.name_scope("read_out"):
+            w_fc2 = self._weight_variable([1024, 42])
+            b_fc2 = self._bias_variable([42])
+
+        # model
+        return tf.add(tf.matmul(fc1_drop, w_fc2), b_fc2, name="model")
